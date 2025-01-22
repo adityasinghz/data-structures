8e3ad85545efc0f0a0760c6c2643f1b27d69460f
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2025-01-22 22:59:47 +0530

    Time: 51 ms (56.47%) | Memory: 29.2 MB (50.23%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,23 +6,24 @@ public:
         for(int i=0;i<nums.size();i++){
             int low = i+1;
             int high = nums.size()-1;
-            while(high>low){
-             if(nums[i] + nums[low] + nums[high] > 0){
-                high--;
-             }else if(nums[i] + nums[low] + nums[high] < 0){
-                low++;
-             }else{
-                res.push_back({nums[low],nums[i],nums[high]});
-                int t1= low, t2 = high;
-                while(low<high && nums[t2]==nums[low]){ 
-                  low++;
-                }
-                while(low<high && nums[t2]==nums[high]){
-                    high--;
-                }
-             }
+
+            while(low < high){
+                 if(nums[i] + nums[low] + nums[high] > 0 ){
+                    high --;
+                 }else if(nums[i] + nums[low] + nums[high] < 0){
+                    low ++;
+                 }else{
+                    res.push_back({nums[i], nums[low], nums[high]});
+                    int t1 = low , t2 = high;
+                    while(low < high && nums[t1] == nums[low]){
+                        low++;
+                    }
+                    while(low < high && nums[t2] == nums[high]){
+                        high--;
+                    }
+                 }
             }
-            while(i+1<nums.size()&& nums[i]==nums[i+1]) i++;
+            while(i+1<nums.size() &&nums[i]==nums[i+1]) i++;
         }
         return res;
     }
