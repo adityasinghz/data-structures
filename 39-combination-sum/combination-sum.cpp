ab83ab2d7e4bb9bc182c3cbc8b700441430127f9
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2025-01-26 20:26:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (99.42%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void knapSack(int n, int target, vector<int>& arr, vector<int>&current, vector<vector<int>>&result){
+        if(target==0){
+            result.push_back(current);
+            return;
+        }
+        if(n==0 || target < 0) return;
+
+
+        if(target >= arr[n-1]){
+          current.push_back(arr[n-1]);
+          knapSack(n, target-arr[n-1], arr, current, result);
+          current.pop_back();
+        }
+        
+        knapSack(n-1,target, arr, current, result);
+    
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end()); 
+        int n = candidates.size();
+        vector<vector<int>>res;
+        vector<int>curr;
+        knapSack(n, target, candidates, curr, res);
+        return res;
+    }
+};
