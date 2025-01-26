d3418f6b3cc13cb1507fdd98be429db23016aede
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2025-01-26 19:12:11 +0530

    Time: 39 ms (94.71%) | Memory: 101.1 MB (21.55%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    bool knapSack(int n, vector<int>&wt, int W, vector<vector<int>>&dp){
-        if(dp[n][W]!=-1) return dp[n][W];
-
-        if(W==0) return true;
-
-        if(wt[n-1]<=W)
-         dp[n][W] = knapSack(n-1, wt , W - wt[n-1], dp) || knapSack(n-1,wt, W, dp);
-        else
-         dp[n][W] = knapSack(n-1, wt, W, dp);
-
-        return dp[n][W];
+    int knapSack(int n, vector<int>&nums, int target, vector<vector<int>>&dp){
+        if(dp[n][target] != -1) return dp[n][target];
+        if(target == 0) return 1;
+        if(nums[n-1] <= target){
+            dp[n][target] = knapSack(n-1, nums, target-nums[n-1], dp) || knapSack(n-1, nums, target, dp);
+        }else{
+            dp[n][target] = knapSack(n-1, nums, target, dp);
+        }
+        return dp[n][target];
     }
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(),nums.end(),0);
-        int W = sum/2;
+        int target = accumulate(nums.begin(), nums.end(),0);
+        if(target%2!=0) return false;
+        target/=2;
         int n = nums.size();
-        vector<vector<int>>dp(n+1, vector<int>(W+1, -1));
-        if(sum%2 != 0) return false;
-        for(int i=0;i<=W;i++){
-            dp[0][i] = false;
-        }
-        for(int i = 0;i<=n;i++){
-           dp[i][0]=true;
-        }
-        
-        knapSack(n , nums, W , dp);
-
-        return dp[n][W];
+        vector<vector<int>>dp(n+1, vector<int>(target+1,-1));
+        for(int i =1; i<=target; i++) dp[0][i] = 0;
+        dp[0][0] = 1;
+        for(int i =0; i<=n;i++) dp[i][0] = 1;
+        return knapSack(n, nums, target, dp);
     }
 };
