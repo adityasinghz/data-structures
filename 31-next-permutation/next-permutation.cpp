da0f443c0eeed180c63ec43ada247cc9b31fe2bd
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2025-03-08 17:41:16 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (5.07%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-       int n = nums.size();
-       int indx = -1;
-       for(int i = n-2;i>=0;i--){
+        int n = nums.size();
+        int indx = -1;
+        // First we need to find first breakpoint from back where it is breaking the decreasing sequence (3,2,1)
+        for(int i = n-2; i>=0 ; i--){
             if(nums[i+1] > nums[i]){
-              indx = i;
-              break;
+               indx=i;
+               break;
             }
-       }
-       if(indx == -1){
-        reverse(nums.begin(),nums.end());
-        return;
-       }
+        }
+        if(indx==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
 
-       for(int i=n-1;i>indx;i--){
-         if( nums[i] > nums[indx]){
-           swap(nums[i],nums[indx]);
-           break;
-         }
-       }
-       reverse(nums.begin()+indx+1 , nums.end());
+        for(int i=n-1;i>indx;i--){  // finding next greater element from the range of last element to wrt indx 
+            if(nums[indx] < nums[i]){
+                swap(nums[i],nums[indx]);
+                break;
+            }
+        }
+        reverse(nums.begin()+indx+1,nums.end());
+
     }
 };
