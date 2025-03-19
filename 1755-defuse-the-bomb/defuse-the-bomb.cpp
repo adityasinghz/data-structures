7c39471738fb1123b57d9ee015422207b2b3f63d
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2025-03-20 04:10:53 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (12.51%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    vector<int> decrypt(vector<int>& nums, int k) {
-        if(k == 0 ) return vector<int>(nums.size(),0);
-        vector<int>ans;
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            int sum = 0;
-            int r = i + k;
-            if(k  > 0){
-                for(int j = i+1; j<=r;j++){
-                   sum+=nums[j%n];
-                }
-            }
-            if(k  < 0){
-                for(int j = i-1; j>=r;j--){
-                   if(j<0)
-                   sum+=nums[n+j%n];
-                   else
-                   sum+=nums[j%n];
-                }
-            }
-            ans.push_back(sum);
+    vector<int> decrypt(vector<int>& code, int k) {
+        // first we're doing pre computation of sum
+        if(k==0) return vector<int>(code.size());
+        int i = 1;
+        int j = k;
+        int n =code.size();
+        vector<int>ans(code.size());
+        if(k < 0){
+            i = (n + k)%n;
+            j = n  - 1;
         }
+
+        int sum = accumulate(code.begin() + i, code.begin() + j+1, 0);
+
+        for(int indx = 0;indx < n; indx++){
+            ans[indx] = sum;
+            sum-=code[i];
+            i = (i+1)%n;
+            j = (j+1)%n;
+            sum+=code[j];
+
+        }
+
         return ans;
     }
 };
-
-// 0 - 100  5
-//  
