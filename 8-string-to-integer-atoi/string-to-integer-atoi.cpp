325f8284ed139f701858a93f31126caaf4172174
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2025-04-18 18:26:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (13.94%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
     int myAtoi(string s) {
-      int i = 0;
-
-      while(s[i]==' '){
-          i++;
-      }
-      int prod = 1;
-      if(s[i]=='-'){ prod  = -1; i++;}
-      else if(s[i]=='+'){ prod = 1; i++;}
-      while(s[i]=='0') i++;
-      if(s[i]-'0' < 0 || s[i]-'0'>9) return 0;
-      long long int res = 0;
-      while(s[i] - '0' >=0 && s[i] - '0' <=9){
-        if((res*10 + s[i] - '0')*prod >= INT_MAX) return INT_MAX;
-        if((res*10 + s[i] - '0')*prod <= INT_MIN) return INT_MIN;
-        res = (res*10 + s[i] - '0');
-        i++;
-      }
-      return res*prod ;
+        string temp;
+        int n = s.size();
+        int j = 0;
+        while(j < n){
+         if(s[j]!=' ')break;
+         j++;
+        }
+        if(s[j]=='+' || s[j]=='-')temp+=s[j++];
+        for(int i=j;i<s.size();i++){
+            char c = s[i];
+            if( (c >= 'a' && c <='z') || (c>='A' && c<='Z') || c=='-' || c=='+'  || c==' ' || c=='.' ){
+              break;
+            }else{
+                temp+=c;
+            }
+        }
+        long long int ans = 0;
+        int i = 0;
+        while(i<temp.size()){
+              if(temp[i]=='+' || temp[i]=='-'){
+               i++;
+               continue;
+              }
+              if(ans*10 + (temp[i] - '0') >= INT_MAX && temp[0]!='-') return INT_MAX;
+              if((ans*10 + (temp[i] - '0'))*-1 <= INT_MIN && temp[0]=='-') return INT_MIN;
+              ans = ans*10 + temp[i] - '0';
+              i++;
+        }
+        cout<<temp<<'\n';
+        return temp[0]=='-'?-ans:ans;
     }
 };
