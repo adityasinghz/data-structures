1e7afcfe2c54884087d1ecf5a55f27242b355889
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2025-04-18 18:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (44.79%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int nn) {
-        long long int n = nn;
-        n = n<0 ? -n : n;
-        double ans = (double)1;
-        while(n > 0){
-             if(n %2 != 0){
-                ans*=x;
-                n--;
-             }else{
-                x*=x;
-                n/=2;
-             }
-        }
-        return nn <0? (double)1/ans : ans;
+       bool isNeg = nn < 0 ? true : false;
+       long long int n = nn;
+       n = n < 0 ? -n : n;
+       double ans = 1;
+       while(n>0){
+          if(n%2==0){
+            x*=x;
+            n/=2;
+          }else{
+            ans*=x;
+            n--;
+          }
+       }
+       return isNeg ? 1/ans:ans;
     }
 };
