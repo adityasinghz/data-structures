441a1a97ad259d882992c8cf84694bf5d1fa0471
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2025-08-11 18:54:43 +0530

    Time: 2 ms (44.77%) | Memory: 115.5 MB (97.99%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -5,9 +5,10 @@ public:
         int lastNum =  INT_MAX;
 
         for(int i=0;i<nums.size();i++){
-            if(firstNum < lastNum && lastNum < nums[i]) return true;
+
             if(firstNum > nums[i]) firstNum = nums[i];
             else if(firstNum < nums[i] && nums[i] < lastNum) lastNum = nums[i];
+            if(firstNum < lastNum && lastNum < nums[i]) return true;
         }
         return false;
     }
