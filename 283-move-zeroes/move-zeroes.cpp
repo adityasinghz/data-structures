1041ede7f673ace80544588fa9accebc718f707e
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2025-08-12 01:24:32 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (54.65%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        if(nums.size()<=1) return;
         int low = 0;
         int high = 0;
-
         while(high < nums.size()){
-             if(nums[high] != 0){
-                 swap(nums[low],nums[high]);
-                 low++;
-             }
-             high++;
+         if(nums[high]!=0)
+           swap(nums[low++], nums[high]);
+        high++;
         }
     }
 };
