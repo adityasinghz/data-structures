d1df65b06bf25efc4d36356f766547221d604b26
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2025-09-20 11:00:08 +0530

    Time: 19 ms (48.28%) | Memory: 26.1 MB (38.37%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>>ans;
-        unordered_map<string, vector<string>> mp;
+        
+        unordered_map<string, vector<string>>freq;
+        vector<vector<string>>res;
         for(auto str : strs){
-             vector<int>counts(26,0);
-             string s;
-             for(int s : str){
-                counts[s-'a']++;
-             }
-             for(int count : counts){
-                s+= ( "#" + to_string(count));
-             }
-             mp[s].push_back(str);
+            string s = str;
+            sort(s.begin() , s.end());
+            freq[s].push_back(str);
         }
-
-        for(auto m : mp){
-            ans.push_back(m.second);
+        for(auto fre : freq){
+            res.push_back(fre.second);
         }
-        return ans;
+        return res;
 
     }
 };
