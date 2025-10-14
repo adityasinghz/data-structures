bc5e6243829c152f02f0d40065c56ba4b5de86dc
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2025-10-14 15:47:49 +0530

    Time: 11 ms (21.57%) | Memory: 24.2 MB (23.34%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -1,18 +1,32 @@
 class NumArray {
 public:
-    vector<int>prefix;
+    vector<int>seg, arr;
+    int n;
     NumArray(vector<int>& nums) {
-        prefix.resize(nums.size());
-        prefix[0] = nums[0];
-        for(int i=1;i<nums.size();i++){
-            prefix[i]= prefix[i-1] + nums[i];
-        }
-
+        seg.resize(4 * nums.size());
+        n = nums.size();
+        arr =  nums;
+        build(0, 0, nums.size() - 1);
     }
-     
+    void build(int i, int low, int high){
+          if(low == high){ seg[i] = arr[low]; return;}
+          int mid = low + (high-low)/2;
+          build(2*i+1, low, mid);
+          build(2*i+2, mid+1,high);
+          seg[i] = seg[2*i+1] + seg[2*i+2];
+    }
+
+    int query(int l, int r, int left, int right, int indx){
+        if(left > r || right < l) return 0;
+
+        if(l>=left && r <= right ) return seg[indx];
+        
+        int mid = l + (r - l)/2;
+return query( l, mid, left, right, 2*indx+1) + query(mid + 1 , r, left, right, 2*indx +2);
+    }
+
     int sumRange(int left, int right) {
-        if(left==0) return prefix[right];
-        return prefix[right] - prefix[left-1];
+     return query(0, n-1, left, right, 0);
     }
 };
 
