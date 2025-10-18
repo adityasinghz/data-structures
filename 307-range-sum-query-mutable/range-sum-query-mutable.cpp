780b02c9c6cb64dd1554bcd4c9a9499c4918e8a6
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2025-10-18 11:02:01 +0530

    Time: 55 ms (72.77%) | Memory: 183.3 MB (12.57%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,55 +1,53 @@
 class NumArray {
 public:
-    vector<int>seg, arr;
+    vector<int>arr, tree;
     int n;
     NumArray(vector<int>& nums) {
-        arr = nums;
         n = nums.size();
-        seg.resize(4*n);
-        build(0, 0, n-1);
+        arr = nums;
+        tree.resize(4*n);
+        build(0,0,n-1);
     }
     void build(int indx, int low, int high){
-        if(low==high){
-            seg[indx] = arr[low];
-            return;
-        }
-
-        int mid = low + (high - low)/2;
-
-        build(2 * indx + 1, low , mid);
-        build(2 * indx + 2, mid+1, high);
-
-        seg[indx] =  seg[2*indx+1] + seg[2*indx + 2];
+       if(low==high){
+          tree[indx] = arr[low];
+          return; 
+       }
+       int mid = low + (high - low)/2;
+       build(2*indx+1,low,mid);
+       build(2*indx+2,mid+1,high);
+       tree[indx] = tree[2*indx+1] + tree[2*indx+2];
     }
+    void update(int indx, int low, int high, int val, int pos){
+       if(low==high){
+           tree[indx] = val;
+           return;
+       }    
+       int mid = low + (high - low)/2;
+       if(pos<=mid){
+         update(2*indx+1, low, mid, val, pos);
+       }else{
+         update(2*indx+2,mid+1,high,val, pos);
+       }
+       tree[indx] = tree[2*indx+1] + tree[2*indx+2];
+    }
+    
+    int query(int indx, int low, int high, int left, int right){
+         if(high < left || right < low) return 0;
 
-    void update(int pos, int val, int low, int high, int indx){
-         if(low==high){
-            seg[indx] =  val;
-            return;
-         }
+         if(left<=low && high<=right)  return tree[indx];
+         
          int mid = low + (high - low)/2;
-         if(pos<=mid){
-            update(pos, val, low, mid, 2*indx+1);
-         }
-         else update(pos, val, mid+1, high, 2*indx+2);
 
-         seg[indx] = seg[2*indx+1] + seg[2*indx+2];
-    }
-    int query(int indx, int left, int right, int low , int high){
-          if(left > high || right < low) return 0;
-          if(left<=low && high<=right) return seg[indx];
-          
-          int mid = low + (high - low)/2;
-
-    return query(2*indx+1, left, right, low, mid) + query(2*indx+2,left, right, mid + 1, high);
+         return query(2*indx+1,low, mid, left, right) + query(2*indx+2,mid+1,high,left,right);
     }
 
     void update(int pos, int val) {
-        update(pos, val, 0, n-1, 0);
+        update(0, 0, n-1, val, pos);
     }
     
     int sumRange(int left, int right) {
-      return query(0, left, right, 0 , n-1);
+        return query(0, 0, n-1, left, right);
     }
 };
 
