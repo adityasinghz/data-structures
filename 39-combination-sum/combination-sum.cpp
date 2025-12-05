0f8dedfcb6c3df19704934bee8faa03867635817
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2025-12-05 21:12:49 +0530

    Time: 2 ms (59.11%) | Memory: 14 MB (90.81%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    void knapSack(int n, int target, vector<int>& arr, vector<int>&current, vector<vector<int>>&result){
-        if(target==0){
-            result.push_back(current);
-            return;
-        }
-        if(n==0 || target < 0) return;
-
-
-        if(target >= arr[n-1]){
-          current.push_back(arr[n-1]);
-          knapSack(n, target-arr[n-1], arr, current, result);
-          current.pop_back();
-        }
-        
-        knapSack(n-1,target, arr, current, result);
-    
+    void backtrack(int n, int target, vector<int>&ds, vector<int>&arr, vector<vector<int>>&res){
+    if(target==0){
+       res.push_back(ds);
+       return;
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-       // sort(candidates.begin(), candidates.end()); 
-        int n = candidates.size();
+    if(target < 0 || n == 0) return;
+     
+    if(arr[n-1]<=target){
+        ds.push_back(arr[n-1]);
+        backtrack(n,target-arr[n-1],ds,arr,res);
+        ds.pop_back();
+    }
+    backtrack(n-1,target,ds,arr,res);
+    }
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+        vector<int>ds;
         vector<vector<int>>res;
-        vector<int>curr;
-        knapSack(n, target, candidates, curr, res);
+        backtrack(arr.size(),target,ds,arr,res);
         return res;
     }
 };
