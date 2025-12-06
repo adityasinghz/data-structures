8620514980c1217b1ec655c4b510629bc78e2708
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2025-12-06 23:29:04 +0530

    Time: 2 ms (82.33%) | Memory: 15.6 MB (63.24%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    void dfs(int open, int close, int n, string params, vector<string>&res){
-        if(open==close && open + close == 2*n){
-            res.push_back(params);
-            return;
-        }
-        if(open < n){
-           dfs(open+1,close,n,params+"(",res);
-        }
-        if(close < open){
-           dfs(open,close+1,n,params+")",res);
-        }
+    void backtrack(int open, int close, int n, string param, vector<string>&res){
+       if(open == close && 2*n==open+close){
+          res.push_back(param);
+          return;
+       }
+       if(open < n) backtrack(open+1,close,n,param+"(",res);
+       if(close<open)backtrack(open,close+1,n,param+")",res);
     }
     vector<string> generateParenthesis(int n) {
-        vector<string>res;
-        dfs(0,0,n,"",res);
-        return res;
+       vector<string>res;
+       backtrack(0,0,n,"",res);
+       return res;     
     }
 };
