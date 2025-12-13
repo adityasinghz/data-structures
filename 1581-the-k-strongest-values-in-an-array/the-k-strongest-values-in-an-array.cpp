0532a03b5999e16247aad29a8b324204f7b15bcc
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2025-12-13 06:03:44 +0530

    Time: 27 ms (85.78%) | Memory: 84.9 MB (93.36%) - LeetSync

--- 1581-the-k-strongest-values-in-an-array/the-k-strongest-values-in-an-array.cpp
+++ 1581-the-k-strongest-values-in-an-array/the-k-strongest-values-in-an-array.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     vector<int> getStrongest(vector<int>& arr, int k) {
-        int n = arr.size();
         sort(arr.begin(),arr.end());
-        priority_queue<pair<int,int>,vector<pair<int,int>>>pq;
-        vector<int>ans(k);
-        int m = (n-1)/2;
-        for(int i=0;i<n;i++){
-            pq.push({abs(arr[i]-arr[m]),arr[i]});
+        vector<int>res;
+        res.reserve(k);
+        int n = arr.size();
+        int mid = arr[(n-1)/2];
+        int low = 0;
+        int high = n-1;
+        while(k--){
+            int lowEle = abs(arr[low]-mid);
+            int highEle = abs(arr[high]-mid);
+            if(lowEle > highEle){ 
+             res.push_back(arr[low++]);
+            }else {
+             res.push_back(arr[high--]);
+            }
         }
-        int i = 0;
-        while(!pq.empty()&&k--){
-           int num = pq.top().second;
-           pq.pop();
-           ans[i++] = num;
-        }
-        return ans;
+        return res;
     }
 };
