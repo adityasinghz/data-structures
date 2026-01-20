8c9ba04f98e3123073eed28e41f6cd5b4caaad5f
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2026-01-20 08:31:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (5.38%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    vector<int>dp = vector<int>(10001,-1);
-    int minCost(int n, vector<int>&cost){
-        if(n - 1 < 0 || n - 2 < 0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = min(cost[n-1] + minCost(n-1, cost), cost[n-2] + minCost(n-2,cost));
+
+    int fibo(int n, vector<int>& cost, vector<int>&dp){
+       if(n - 1 < 0 || n - 2 < 0) return 0;
+       if(dp[n]!=-1) return dp[n];
+       return dp[n] = min(cost[n-1] + fibo(n-1,cost,dp) ,cost[n-2] + fibo(n-2,cost,dp));
     }
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        return minCost(n, cost);
+        vector<int>dp(1001,-1);
+        return fibo(cost.size(),cost, dp);
     }
 };
