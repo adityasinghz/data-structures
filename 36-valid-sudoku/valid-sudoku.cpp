9c2dd966fda747bf9e035a9fc355caa4b430a66f
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2026-03-16 18:50:11 +0530

    Time: 19 ms (5.29%) | Memory: 24 MB (39.27%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,50 +1,41 @@
 class Solution {
 public:
-    bool rowCheck(int row, int col, vector<vector<char>>& board, unordered_map<char,bool>freq){
-         for(int i=row;i<board.size();i++){
-             if(!isdigit(board[i][col])) continue;
-             if(freq[board[i][col]]) return false;
-             freq[board[i][col]] = true;
-         }
-         return true;
-    }
-
-    bool colCheck(int row, int col, vector<vector<char>>& board, unordered_map<char,bool>freq){
-            for(int j=col;j<board[0].size();j++){
-             if(!isdigit(board[row][j])) continue;
-             if(freq[board[row][j]]) return false;
-             freq[board[row][j]] = true;
-            }
-            return true;
-    }
-
-
     bool isValidSudoku(vector<vector<char>>& board) {
-        int m = board.size();
-        int n = board[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                unordered_map<char,bool>freq;
-                bool rowValid = rowCheck(i, j,board, freq);
-                bool colValid = colCheck(i, j,board, freq);
-                if(!rowValid) return false;
-                if(!colValid) return false;
+       
+        for(int i=0;i<9;i++){
+            unordered_map<char,bool>rows;
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.') continue;
+                if(rows[board[i][j]]) return false;
+                rows[board[i][j]] = true;
             }
         }
-        cout<<"alive\n";
-        for(int boxRow=0;boxRow<m;boxRow+=3){
-            for(int boxCol=0;boxCol<n;boxCol+=3){
-            unordered_map<char,bool>freq;
-            for(int i=boxRow;i<boxRow+3;i++){
-                for(int j=boxCol;j<boxCol+3;j++){
-                    if(!isdigit(board[i][j])) continue;
-                    if(freq[board[i][j]]) return false;
-                    freq[board[i][j]] = true;
+        cout<<"Rows\n";
+        
+        for(int j = 0;  j < 9; j++){
+            unordered_map<char,bool>cols;
+            for(int i = 0; i < 9; i++){
+                if(board[i][j]=='.') continue;
+                if(cols[board[i][j]]) return false;
+                cols[board[i][j]] = true;
+            }
+        }
+        
+        cout<<"Cols\n";
+        for(int i = 0; i < 9; i+=3){
+            for(int j = 0 ; j < 9; j+=3){
+                unordered_map<char, bool>grids;
+                cout<<i<<' '<<j<<'\n';
+                for(int row = i; row < i + 3; row++){
+                    for(int col = j; col < j + 3; col++){
+                       if(board[row][col]=='.') continue;
+                       if(grids[board[row][col]]) return false;
+                       grids[board[row][col]] = true;
+                    }
                 }
             }
-            cout<<'\n';
-            }
-        }    
+        }
+        cout<<"Grid\n";
         return true;
     }
 };
