59f77be0954439d6f2ce865f4465487ed7620836
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2026-03-19 18:16:49 +0530

    Time: 39 ms (95.06%) | Memory: 29 MB (71.95%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-          sort(nums.begin(),nums.end());
-          vector<vector<int>>ans;
-          int n = nums.size();
-          for(int i=0;i<nums.size();i++){
-              int low = i + 1;
-              int high = nums.size() - 1;
-              while(low < high){
-                  int sum = nums[low] + nums[high] + nums[i];
-                  if(sum > 0) high--;
-                  else if(sum < 0) low++;
-                  else{
-                      ans.push_back({nums[i], nums[low], nums[high]});
-                      int temp_l = low;
-                      int temp_h = high;
-                      while(low < high && nums[low]==nums[temp_l])low++;
-                      while(low < high && nums[high]==nums[temp_h])high--; 
-                  }   
-              }
-              int temp_i = i;
-              while(i+1 < n && nums[i+1] == nums[temp_i])i++;
-          }
-          return ans;
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>res;
+        int n  = nums.size();
+        for(int i=0;i<nums.size();i++){
+            int low = i+1;
+            int high = nums.size() - 1;
+            while(low < high){
+                if(nums[low] + nums[high] + nums[i] > 0) high--;
+                else if(nums[low] + nums[high] + nums[i] < 0) low++;
+                else {
+                    res.push_back({nums[i], nums[low], nums[high]});
+                    do{low++;} while(low+1 < n && nums[low]==nums[low-1]);
+                    do{high--;} while(high-1 >=0 && nums[high]==nums[high+1]);
+                }
+            }
+            while(i+1 < n && nums[i]==nums[i+1]) i++;
+        }
+        return res;
     }
 };
