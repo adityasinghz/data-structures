9ab3ff2f86b49776da79b1a54b088f38071cddfe
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2026-05-17 17:40:12 +0530

    Time: 693 ms (15.56%) | Memory: 295.6 MB (18.61%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
-   void merge(vector<int>&nums, int low, int mid, int high){
+    void merge(int low, int mid, int high, vector<int>&arr){
+        vector<int>aux;
         int i = low;
-        int j = mid+1;
+        int j = mid + 1;
+    
+        while( i<=mid && j<=high){
+            if(arr[i] < arr[j]) aux.push_back(arr[i++]);
+            else aux.push_back(arr[j++]);
+        }
+        while( i <=mid)  aux.push_back(arr[i++]);
+        while( j <=high) aux.push_back(arr[j++]);
+
         int k = low;
-        int aux[high+1];
-        while(i<=mid && j<=high){
-           if(nums[i] > nums[j])
-              aux[k++] = nums[j++];
-           else
-              aux[k++] = nums[i++];
+        for(int l=0;l<aux.size();l++){
+            arr[k++] = aux[l];
         }
+    }
 
-        while(i<=mid){
-            aux[k++] = nums[i++];
-        }
-        
-        while(j<=high){
-            aux[k++] = nums[j++];
-        }
-
-        for(int l = low; l<=high; l++)
-          nums[l] = aux[l];
-   }
-   void mergeSort(vector<int>&nums, int low, int high){
+    void mergeSort(int low, int high, vector<int>&arr){
+        int mid = low + (high - low)/2;
         if(low < high){
-            int mid = low + (high - low)/2;
-            mergeSort(nums, low, mid);
-            mergeSort(nums, mid+1, high);
-            merge(nums,low, mid, high);
+           //cout<<low<<' '<<high<<'\n';
+           mergeSort(low, mid, arr);
+           mergeSort(mid+1, high, arr);
+           merge(low, mid, high, arr);  
         }
-   }
+    }
     vector<int> sortArray(vector<int>& nums) {
-       int n = nums.size();
-       mergeSort(nums, 0 , n-1);
-       return nums;
+        int n = nums.size();
+        mergeSort(0, n-1, nums);
+        return nums;
     }
 };
