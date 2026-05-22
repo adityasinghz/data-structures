283267037d8e4fd3d46304a3f6f28507bd93904b
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2026-05-22 19:11:31 +0530

    Time: 48 ms (73.97%) | Memory: 71 MB (83.58%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    void merge(int low, int mid, int high, vector<int>&arr){
-        vector<int>aux;
-        int i = low;
-        int j = mid + 1;
-    
-        while( i<=mid && j<=high){
-            if(arr[i] < arr[j]) aux.push_back(arr[i++]);
-            else aux.push_back(arr[j++]);
-        }
-        while( i <=mid)  aux.push_back(arr[i++]);
-        while( j <=high) aux.push_back(arr[j++]);
+    void heapify(int i , int n, vector<int>&arr){
+        int left   = 2*i + 1;
+        int right  = 2*i + 2;
 
-        int k = low;
-        for(int l=0;l<aux.size();l++){
-            arr[k++] = aux[l];
+        int largest = i;
+
+        if(left < n && arr[left] > arr[largest])
+          largest = left;
+
+        if(right < n && arr[right] > arr[largest])
+          largest = right;
+        
+        if(i!=largest){
+            swap(arr[i], arr[largest]);
+            heapify(largest, n, arr);
         }
     }
 
-    void mergeSort(int low, int high, vector<int>&arr){
-        int mid = low + (high - low)/2;
-        if(low < high){
-           //cout<<low<<' '<<high<<'\n';
-           mergeSort(low, mid, arr);
-           mergeSort(mid+1, high, arr);
-           merge(low, mid, high, arr);  
-        }
-    }
     vector<int> sortArray(vector<int>& nums) {
+
         int n = nums.size();
-        mergeSort(0, n-1, nums);
+
+        for(int i=n/2-1; i>=0; i--){
+              heapify(i,n,nums);
+        }
+        
+        for(int i = n-1; i>0; i--){
+            swap(nums[i], nums[0]);
+            heapify(0, i, nums);
+        }
+
         return nums;
     }
 };
