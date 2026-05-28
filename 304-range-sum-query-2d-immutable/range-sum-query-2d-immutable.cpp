6c1d2b7737cf6743d11bfa9bbdb3ef0ade1d50df
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2026-05-28 23:39:41 +0530

    Time: 26 ms (48.30%) | Memory: 149.2 MB (59.62%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -1,31 +1,29 @@
 class NumMatrix {
 public:
-    vector<vector<int>>prefixSum;
+    vector<vector<int>>subMat;
     NumMatrix(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-
-        prefixSum = vector<vector<int>>(rows,vector<int>(cols,0));
-        
-        for(int i=0; i < rows; i++){
-            prefixSum[i][0] = matrix[i][0];
-            for(int j = 1; j < cols; j++){
-                prefixSum[i][j] = prefixSum[i][j-1] + matrix[i][j];
+        int row = matrix.size(); int col = matrix[0].size();
+        subMat = vector<vector<int>>(row+1,vector<int>(col+1,0));
+        for(int i=0;i<row;i++){
+            int prefix = 0;
+            for(int j=0;j<col;j++){
+               prefix+=matrix[i][j];
+               int above = subMat[i][j+1];
+               subMat[i+1][j+1] = prefix + above;
             }
         }
-
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int res = 0;
-        for(int i=row1;i<=row2;i++){
-            if(col1 > 0){
-                res+=prefixSum[i][col2] - prefixSum[i][col1-1];  
-            }else{
-                res+=prefixSum[i][col2];
-            }
-        }
-        return res;
+        row1++; col1++; row2++; col2++;
+
+        int bottomRight = subMat[row2][col2];
+        int topLeft = subMat[row1-1][col1-1];
+        int topRight = subMat[row1-1][col2];
+        int bottomLeft = subMat[row2][col1-1];
+
+        return bottomRight - topRight - bottomLeft + topLeft;
+        
     }
 };
 
@@ -33,8 +31,4 @@ public:
  * Your NumMatrix object will be instantiated and called as such:
  * NumMatrix* obj = new NumMatrix(matrix);
  * int param_1 = obj->sumRegion(row1,col1,row2,col2);
-
-1, 2, 3, 4, 5
-
-1, 
  */
