fc965a35af57420678970c218245fa89a38e94df
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2026-06-02 10:17:50 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (99.92%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int firstEle = INT_MIN;
-        int secondEle = INT_MIN;
+        int firstEle = -1;
+        int secondEle = -1;
 
-        int countFirst = 0;
-        int countSecond = 0;
-        
-        int n = nums.size();
+        int firstCount = 0;
+        int secondCount = 0;
 
-        for(int i = 0; i < n; i++){
-            if(firstEle==nums[i])countFirst++;
-            else if(secondEle==nums[i])countSecond++;
-            else if(countFirst==0) {firstEle = nums[i]; countFirst=1;}
-            else if(countSecond==0){ secondEle = nums[i]; countSecond=1;} 
-            else{
-                countSecond--;
-                countFirst--;
+        for(int i=0;i<nums.size();i++){
+            if(firstEle == nums[i]){
+               firstCount++;
+            }else if(secondEle==nums[i]){
+              secondCount++;
+            }else if(firstCount==0){
+              firstEle = nums[i];
+              firstCount=1;
+            }else if(secondCount==0){
+             secondEle = nums[i];  
+             secondCount=1;
+            }else{
+                firstCount--;
+                secondCount--;
             }
         }
-        vector<int>aux;
+        vector<int>res;
         int ct1 = 0;
-        int ct2 = 0; 
-        for(int i=0;i<n;i++){
-             if(nums[i] == firstEle) ct1++;
-             if(nums[i] == secondEle) ct2++;
+        int ct2 = 0;
+        cout<<firstEle<<' '<<secondEle<<'\n';
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==firstEle) ct1++;
+            else if(nums[i]==secondEle) ct2++;
         }
 
-        if(ct1 > n/3) aux.push_back(firstEle);
-        if(ct2 > n/3) aux.push_back(secondEle);
-
-        return aux;
+        if(ct1 > nums.size()/3) res.push_back(firstEle);
+        if(ct2 > nums.size()/3) res.push_back(secondEle); 
+        return res;
     }
 };
