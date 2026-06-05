9f3c9a2fd9e2efbae46a7b5f2b8b45fc4903b687
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2026-06-05 20:11:46 +0530

    Time: 44 ms (75.09%) | Memory: 28.9 MB (99.51%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
         vector<vector<int>>res;
-        for(int i=0; i < n; i++){
-                int low = i+1;
-                int high = n - 1;
-                while( low < high){
-                    if(nums[i] + nums[low] + nums[high] > 0) high--;
-                    else if(nums[i] + nums[low] + nums[high] < 0) low++;
-                    else{
-                        res.push_back({nums[i],nums[low],nums[high]});
-                        int temp_l = nums[low];
-                        int temp_h = nums[high];
-
-                        do{low++;}while(low < high && temp_l==nums[low]);
-                        do{high--;}while(low < high && temp_h==nums[high]);
-                    }
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            int low = i+1;
+            int high = n - 1;
+            while(low < high){
+                if( nums[i] + nums[low] + nums[high] > 0) high--;
+                else if( nums[i] + nums[low] + nums[high] < 0) low++;
+                else{
+                   res.push_back({nums[i], nums[low], nums[high]});
+                   int temp_low = nums[low];
+                   int temp_high = nums[high];
+                   do{low++;}while(low < high && temp_low == nums[low]);
+                   do{high--;}while(low < high && temp_high==nums[high]);
                 }
+            }
             int temp_i = nums[i];
-            while( i + 1 < n && nums[i+1] == temp_i) i++;
+            while(i + 1 < n && temp_i == nums[i+1]) i++;
         }
         return res;
     }
