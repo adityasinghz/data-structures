cd71b99da7e11f05f1269310942e84b7cc4b6b80
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2026-06-12 00:08:44 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (46.32%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        if(s1.size() > s2.size()) return false;
         int m = s1.size();
         int n = s2.size();
         int i = 0;
         int j = 0;
-        sort(s1.begin(),s1.end());
+        vector<int>f1(26,0);
+        vector<int>f2(26,0);
+        for(auto c : s1) f1[c-'a']++;
+
         while( j < n){
-           while(j - i + 1 < m) j++;
-           if(j - i + 1 == m){
-            string subString = s2.substr(i, j - i + 1);
-            sort(subString.begin(),subString.end());
-            if(s1 == subString) return true;
-           }
+           while(j - i + 1 <= m){f2[s2[j]-'a']++; j++;}
+           if(f1 == f2) return true;
+           f2[s2[i]-'a']--;
            i++;
         }
         return false;
