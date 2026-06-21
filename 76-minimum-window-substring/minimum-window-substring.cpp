11a86d607e5647280f3d3c255089f71ad09f230c
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2026-06-21 17:35:33 +0530

    Time: 800 ms (5.02%) | Memory: 11.5 MB (42.96%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
-    bool checkMap(vector<int>& a, vector<int>& b) {
-        for (int i = 0; i < 128; i++) {
-            if (a[i] > b[i])
-                return false;
-        }
-        return true;
-    }
-    void checkMinStr(long long& firstPoint, long long& minLen, long long& lastPoint, int& initialMin,
-                     string& s, vector<int>& a, vector<int>& b) {
-        if (lastPoint - firstPoint + 1 < minLen) {
-            minLen = min(minLen, lastPoint - firstPoint + 1);
-            initialMin = firstPoint; 
-        }
-    }
     string minWindow(string s, string t) {
-        long long int firstPoint = 0;
-        long long int lastPoint = 0;
-        int initialMin = INT_MAX;
-        long long int n = s.size();
-        long long int m = t.size();
-        vector<int> a(128, 0), b(128, 0);
-        for (long long i = 0; i < m; i++) a[t[i]]++;
-        for (; firstPoint < n; firstPoint++) if (a[s[firstPoint]]) break;
-        if (firstPoint == n) return "";
-        
-        lastPoint = firstPoint;
-        long long minLen = INT_MAX;
-        string res;
-        while (lastPoint < n) {
-            if (a[s[lastPoint]]) b[s[lastPoint]]++;
-            while(checkMap(a, b)) {
-                checkMinStr(firstPoint, minLen, lastPoint, initialMin, s, a, b);
-                if(a[s[firstPoint]])b[s[firstPoint]]--;++firstPoint;
-                while(firstPoint < lastPoint && !a[s[firstPoint]])firstPoint++;
+        if(s == t) return s;
+        int i = 0;
+        int j = 0;
+        int m = s.size();
+        int n = t.size();
+        if ( n == 0 || m == 0 || n > m) return "";
+        int ct = 0;
+        int minL = INT_MAX;
+        int minI = -1;
+        int minJ = -1;
+        unordered_map<char,int>mp;
+        for(auto c : t) mp[c]++;
+        while( j < m){
+            if(mp[s[j]] > 0){
+                ct++;
             }
-            ++lastPoint;
-        }
-        return minLen == INT_MAX ? "" : s.substr(initialMin, minLen);
+            while( i <= j && ct==n){
+                if(minL > j - i + 1){
+                   minI = i;
+                   minJ = j;
+                   minL = j - i + 1;
+                }
+                cout<<i<<' '<<j<<'\n';
+                if(mp[s[i]] >= 0) ct--;
+                mp[s[i]]++;
+                i++;
+            }
+            mp[s[j]]--;
+            j++;
+        } 
+        return minL==INT_MAX ? "" : s.substr(minI, minL);
     }
 };
