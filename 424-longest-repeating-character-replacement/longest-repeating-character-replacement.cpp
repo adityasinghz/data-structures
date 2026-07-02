19a9b03a703e2b3b701a3cbcb8787e74b164de84
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2026-07-03 00:46:05 +0530

    Time: 8 ms (38.58%) | Memory: 11 MB (19.23%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n = s.size();
-        int maxFreq  = 0;
-        int maxLen = 0;
-        int i = 0;
         int j = 0;
-        unordered_map<char,int>freq;
+        int i = 0;
+        int n = s.size();
+        int maxLen= 0;
+        vector<int>freq(26,0);
         while( j < n ){
-            freq[s[j]]++;
-            maxFreq = max(maxFreq, freq[s[j]]);
-            int rem =  j - i + 1 - maxFreq;
+            freq[s[j] - 'A']++;
+            int maxFreq = 0;
 
-            while(rem > k){ 
-                freq[s[i]]--;
-                int tempMaxFreq = 0;
-                for(auto m : freq) tempMaxFreq = max(tempMaxFreq, m.second);
-                i++;
-                rem =  j - i + 1 - tempMaxFreq; 
+            for(auto f : freq)
+               maxFreq = max(f, maxFreq);
+
+            while( ( j - i + 1 - maxFreq ) > k ){
+                 freq[s[i] - 'A']--;
+                 i++;
             }
             maxLen = max(maxLen, j - i + 1);
             j++;
         }
-
         return maxLen;
+
     }
 };
