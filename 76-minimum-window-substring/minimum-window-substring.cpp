fb23f0b3221ab24ee1da8114d00a459fb4cf241d
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2026-07-10 08:38:15 +0530

    Time: 8 ms (55.14%) | Memory: 11.4 MB (65.40%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if(s == t) return s;
-        int i = 0;
+        unordered_map<char, int> freq;
+
+        for (auto c : t)
+            freq[c]++;
+
         int j = 0;
-        int m = s.size();
-        int n = t.size();
-        if ( n == 0 || m == 0 || n > m) return "";
+        int i = 0;
+        int n = s.size();
+        int m = t.size();
         int ct = 0;
-        int minL = INT_MAX;
-        int minI = -1;
-        int minJ = -1;
-        unordered_map<char,int>mp;
-        for(auto c : t) mp[c]++;
-        while( j < m){
-            if(mp[s[j]] > 0){
+        int initialI = -1;
+        int minLen = INT_MAX;
+        while (j < n) {
+            if (freq[s[j]] > 0) {
                 ct++;
             }
-            while( i <= j && ct==n){
-                if(minL > j - i + 1){
-                   minI = i;
-                   minJ = j;
-                   minL = j - i + 1;
-                }
-                cout<<i<<' '<<j<<'\n';
-                if(mp[s[i]] >= 0) ct--;
-                mp[s[i]]++;
-                i++;
+            freq[s[j]]--;
+            if (ct == m) {
+                
+                do {
+                    if (j - i + 1 < minLen) {
+                    minLen = min(minLen, j - i + 1);
+                    initialI = i;
+                    }
+                    if (freq[s[i]] == 0) ct--;
+                    
+                    ++freq[s[i]];
+                    i++;
+                    
+                } while (ct == m);
+                
             }
-            mp[s[j]]--;
+
             j++;
-        } 
-        return minL==INT_MAX ? "" : s.substr(minI, minL);
+        }
+        return initialI > -1 ? s.substr(initialI, minLen) : "";
     }
 };
