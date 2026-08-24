6ca44a62f8b5e9cb8c9cc7f1437ff1af5e08f05c
Aditya Singh <69703250+adityasinghz@users.noreply.github.com>
2026-08-24 16:35:05 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (25.99%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>&grid, vector<vector<int>>&dp){
-        if( i  == grid.size() - 1) return grid[i][j];
-        if( dp[i][j] != INT_MIN) return dp[i][j];
-        return dp[i][j] = grid[i][j] + min(solve( i + 1 , j, grid, dp), solve(i + 1, j + 1, grid,dp));
+    int solve(int i, int j, int m, vector<vector<int>>&triangle, vector<vector<int>>&dp){
+        if( i == m - 1   ) return triangle[i][j];
+        if(dp[i][j]!=INT_MAX) return dp[i][j];
+        int down = solve(i+1,j,m,triangle, dp);
+        int right = solve(i+1,j+1,m,triangle, dp);
+
+        return dp[i][j] = triangle[i][j] + min(down, right);
     }
-    int minimumTotal(vector<vector<int>>& grid) {
-        vector<vector<int>>dp(grid.size());
-        for(int i = 0; i < grid.size(); i++) dp[i] = vector<int>(grid[i].size(), INT_MIN);
-        
-        return solve(0,0,grid,dp);
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m = triangle.size();
+        vector<vector<int>>dp(m,vector<int>(m,INT_MAX));
+        return solve(0, 0,m, triangle, dp);
     }
 };
